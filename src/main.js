259fe51589766c0c523c6b61ef7567701def6f84
metinelitas <metinelitas@gmail.com>
2024-04-03 12:59:39 +0200

    Workers

--- src/main.js
+++ src/main.js
@@ -1,32 +1,11 @@
-import { runtime, devTools } from "@raydiant/sdk";
+import { workers } from "@raydiant/sdk";
 
-// Open the console and run devTools.play() to start play the video.
-// Enable looping to simulate a single presentation in the playlist
-// by running devTools.play(true).
-window.devTools = devTools;
-
-// Create the video element and add it to the DOM after setting
-// up event listeners. This ensures the canplay listener is always
-// fired, triggering runtime.ready().
-const video = document.createElement("video");
-video.setAttribute('class', 'video')
-
-video.addEventListener("ended", () => {
-  runtime.complete();
+const perchviewDiv = document.querySelector('#perchview');
+workers.subscribe('perchview', (state) => {
+	perchviewDiv.textContent = JSON.stringify(state, null, '  ');
 });
 
-video.addEventListener("error", () => {
-  runtime.complete(new Error("Playback error"));
+const aiDiv = document.querySelector('#ai');
+workers.subscribe('ai-realtime', (state) => {
+	aiDiv.textContent = JSON.stringify(state, null, '  ');
 });
-
-runtime.subscribe("play", () => {
-  video.play();
-});
-
-video.src = "/video.mp4";
-
-// Attach video element to DOM.
-const videoContainer = document.querySelector(".video-container");
-if (!videoContainer) throw new Error("Missing video container element.");
-
-videoContainer.appendChild(video);
